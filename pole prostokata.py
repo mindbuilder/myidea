14d3a1a7baa302aa5785f1b6c40dfae1c7dc3740
mindbuilder <marek.duda1984@gmail.com>
2026-03-15 12:03:21 +0100

    pole

---
+++ pole prostokata.py
@@ -0,0 +1,5 @@
+dlugosc = float(input("Podaj długość prostokąta: "))
+szerokosc = float(input("Podaj szerokość prostokąta: "))
+
+pole = dlugosc * szerokosc
+print("Pole powierzchni prostokąta wynosi:", pole)

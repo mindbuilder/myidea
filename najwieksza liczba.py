ac8cc59b7e66024891e6b8ec59df2febf91afb2a
mindbuilder <marek.duda1984@gmail.com>
2026-03-15 12:07:00 +0100

    najwieksza liczba

---
+++ najwieksza liczba.py
@@ -0,0 +1,7 @@
+lista_liczb = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+
+najmniejsza = min(lista_liczb)
+najwieksza = max(lista_liczb)
+
+print("Najmniejsza liczba w liście:", najmniejsza)
+print("Największa liczba w liście:", najwieksza)
